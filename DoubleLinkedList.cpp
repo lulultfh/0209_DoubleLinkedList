52dd22dd7839c0f0300e27e752c2a70b6e384dbc
lulultfh <punyalulu05@gmail.com>
2024-05-03 23:32:40 +0700

    add perintah untuk memasukkan data nimMhs dan name ke data field di node baru

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -15,4 +15,8 @@ Node* START = NULL; //menandakan node masih kosong
 void addNode()
 {
     Node *newNode = new Node(); //step 1: create a new node
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode-> noMhs; //assign value to the data field of the new node
+    cout << "\nEnter the roll name of the student: ";
+    cin >> newNode-> name; //assign value to the data field of the new node
 }
