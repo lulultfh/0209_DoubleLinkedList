e1f719c4b2eb1bfd88cb2ae2f072329c53c5a396
lulultfh <punyalulu05@gmail.com>
2024-05-04 21:10:52 +0700

    add procedure traverse

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -138,3 +138,23 @@ bool listEmpty()
 {
     return (START == NULL);
 }
+
+void traverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else 
+    {
+        cout << "\nRecords in descending order of roll number are: " << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+        
+    }
+}
