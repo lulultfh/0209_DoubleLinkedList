cf80a8094c2fd628d671c3faaf3582eaef703719
lulultfh <punyalulu05@gmail.com>
2024-05-04 20:28:35 +0700

    add decision making 'else' (if previous still NULL)

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -61,5 +61,10 @@ void addNode()
         {
             previous->next = newNode; //step 7: make the next field of the previous node point to the new node
         }
+        else
+        {
+            //If previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
     }
 }
