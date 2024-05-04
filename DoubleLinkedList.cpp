7a63257cba1ab9b94b63949877d468e97698d8f2
lulultfh <punyalulu05@gmail.com>
2024-05-04 20:18:15 +0700

    add step 6

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -51,5 +51,10 @@ void addNode()
 
         newNode->next = current;    //step 4: make the next field of the new node point to current 
         newNode->prev = previous;   //step 5: make the previous fieldof the new node point to previous
+
+        if (current != NULL)
+        {
+            current->prev = newNode; //step 6: make the previous field of the current node point to the new node
+        }
     }
 }
