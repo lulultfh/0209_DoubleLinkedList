0f0864b5d989b340977133cf07fa777e008d626f
lulultfh <punyalulu05@gmail.com>
2024-05-04 21:01:02 +0700

    add procedure deleteNode (untuk menghapus node yang diinginkan)

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -80,3 +80,56 @@ bool search(int rollNo, Node **previous, Node **current)
     }
     return (*current != NULL);
 }
+
+void deleteNode()
+{
+    Node *previous, *current; 
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo; //step 3: get the roll number to be deleted
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    current = START; //step 1 : start from the first node
+    previous = NULL;
+
+    //Locate the node to be deleted
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number" << rollNo << "not found\033[0m" <<endl;
+        return;
+    }
+
+    //Node to be deleted is the first node
+    if (current == START)
+    {
+        START = START->next; // step 2: update the START pointer
+        if (START != NULL)
+        {
+            START->prev = NULL; //step 3: Assign NULL to the prev field of the node marked as START
+        }
+    }
+    else
+    {
+        //Node to be deleted is not the first node
+        previous->next = current->next;
+        if (current->next != NULL)
+        {   //if there's a successor, update its prev pointer
+            current->next->prev=previous; //step 3 in deleting node between two node in the list
+        }
+    }
+
+    //release the memory of the node marked as current [step 4, in begin, between and end]
+    delete current;
+    cout << "\x1b[32mRecord with roll number " << rollNo << "deleteed\x1b[0m" << endl;
+}
